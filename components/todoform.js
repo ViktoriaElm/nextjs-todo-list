a43282a8abe1ce9c114dfd406faea89fcc1a0371
ViktoiriaElm <v.gorkina.job@gmail.com>
2023-03-08 23:51:58 +0300

    31 строка добавила функцию очищения всего списка

--- components/todoform.js
+++ components/todoform.js
@@ -1,24 +1,23 @@
 import { useCallback, useState } from "react"
 
 export default function ToDoForm({ addTask }) {
-    console.count('ToDoForm')
-    const [userInput, setUserInput] = useState('')
-
+    const [userInput, setUserInput] = useState('');
+    
     const handleSubmit = useCallback((e) => {
         e.preventDefault()
         addTask(userInput)
         setUserInput("")
-    })
+    });
 
     const handleChange = useCallback((e) => {
         setUserInput(e.currentTarget.value)
-    })
+    });
 
     const handleKeyPress = useCallback((e) => {
         if (e.key === "Enter") {
             handleSubmit(e)
         }
-    })
+    });
 
     return (
         <form onSubmit={handleSubmit} >
--- pages/index.js
+++ pages/index.js
@@ -9,23 +9,28 @@ export default function App() {
     const addTask = useCallback((userInput) => {
         if (userInput) {
             const newItem = {
-                id: Math.random().toString(36).substr(2, 9),
+                id: Date.now(),
                 task: userInput,
                 complete: false
             }
             setTodos([...todos, newItem])
         }
-    })
+    });
 
     const removeTask = useCallback((id) => {
         setTodos([...todos.filter((todo) => todo.id !== id)])
-    })
+    });
 
     const handleToggle = useCallback((id) => {
         setTodos([
             ...todos.map((todo) =>
                 todo.id === id ? { ...todo, checked: !todo.checked } : { ...todo })
         ])
+    });
+
+    const clearAll = useCallback(() => {
+        setTodos([]);
+        todos.length = 0;
     })
 
     return (
@@ -37,6 +42,7 @@ export default function App() {
 
             <ToDoForm addTask={addTask} />
             <h3>Осталось выполнить: {todos.length}</h3>
+            <button className="clearAll" onClick={clearAll}>Очистить все</button>
 
             <div className="task">
 
